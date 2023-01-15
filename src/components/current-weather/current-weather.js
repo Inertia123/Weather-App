dc0805b8393d763a462b0f8d9a3114c6da7fd37a
Inertia123 <you@example.com>
2023-01-15 18:05:27 +0530

    weather component done

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,7 +4,23 @@ const CurrentWeather = () => {
     return (
         <div className="weather" >
             <div className="right" >
-                
+                <h1 className="city">Other Detalis</h1>
+                <div className="para">
+                    <span className="parameter label">Feels Like:</span>
+                    <span className="parameter value">18°C</span>
+                </div>
+                <div className="para">
+                    <span className="parameter label">Wind:</span>
+                    <span className="parameter value">2 m/s</span>
+                </div>
+                <div className="para">
+                    <span className="parameter label">Humidity:</span>
+                    <span className="parameter value">15%</span>
+                </div>
+                <div className="para">
+                    <span className="parameter label">Pressure:</span>
+                    <span className="parameter value">18 hPa</span>
+                </div>
             </div>
             <div className="left" >
                 <h1 className="city">Bhubaneswar</h1>
@@ -15,7 +31,7 @@ const CurrentWeather = () => {
                 <div className="temp">
                     <p className="temperature">20°</p>
                     <div className="pictorial">
-                        <img alt="weather" className="weather-icon" src=""/>
+                        <img alt="weather" className="weather-icon" src="icons/01d.png"/>
                         <p className="weather-description">Sunny</p>
                     </div>
                 </div>
